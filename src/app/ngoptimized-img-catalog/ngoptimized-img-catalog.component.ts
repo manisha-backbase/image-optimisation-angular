fc3e58b5f8c53b241af752fe2c6b427f8b3516ef
manisha <manisha@backbase.com>
2023-05-05 16:29:13 +0200

    testing

--- src/app/ngoptimized-img-catalog/ngoptimized-img-catalog.component.ts
+++ src/app/ngoptimized-img-catalog/ngoptimized-img-catalog.component.ts
@@ -7,17 +7,13 @@ import { Component } from '@angular/core';
 })
 export class NgoptimizedImgCatalogComponent {
 
-  public imgList = [{ name: "custom-img.webp", width: 400, height: 400, priority: true },
-  { name: "business-img.webp", width: 300, height: 300, priority: true },
-  { name: "holidays-img.webp", width: 400, height: 400, priority: true },
-  { name: "new-phone-img.webp", width: 400, height: 400, priority: true },
-  { name: "savings-img.webp", width: 400, height: 400, priority: true },
-  { name: "gift-img.webp", width: 400, height: 400, priority: true },
-  { name: "new-car-img.webp", width: 400, height: 400, priority: true },
-  { name: "insurance-img.webp", width: 400, height: 400, priority: true },
-  { name: "home-img.webp", width: 400, height: 400, priority: true },
-  { name: "rent-img.webp", width: 400, height: 400, priority: true },
-  { name: "manage-pockets-empty-state-img.webp", width: 400, height: 400, priority: true }]
+  public imgList = [{ name: "img1.jpeg", width: 400, height: 400, priority: true },
+  { name: "img2.jpeg", width: 300, height: 300, priority: true },
+  { name: "img3.jpeg", width: 400, height: 400, priority: true },
+  { name: "img4.jpeg", width: 400, height: 400, priority: true },
+  { name: "img5.jpeg", width: 400, height: 400, priority: true },
+  { name: "img6.jpeg", width: 400, height: 400, priority: true },
+  { name: "img7.jpeg", width: 400, height: 400, priority: true }]
   catalogImageDir(imgName: string): string {
     return `./assets/content/${imgName}-img.webp`;
   }
